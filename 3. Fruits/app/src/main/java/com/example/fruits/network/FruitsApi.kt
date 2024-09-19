b4535fe90c71591ebddafa3cfc70809060b803f6
Juan Acevedo <juanjo210504@gmail.com>
2024-09-19 16:39:47 -0500

    Base para realizar el proyecto segun el ejemplo en clase

--- 3. Fruits/app/src/main/java/com/example/fruits/network/FruitsApi.kt
+++
@@ -1,9 +0,0 @@
-package com.example.fruits.network
-
-import com.example.fruits.models.Fruit
-import retrofit2.http.GET
-
-interface FruitsApi {
-    @GET("fruit/all")
-    suspend fun getAllFruits(): List<Fruit>
-}
